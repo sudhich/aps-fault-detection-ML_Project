6bb8ea2fe26b1cebb9d3c37012e9d23cae493655
sudhich <sudhich1208@gmail.com>
2022-12-14 17:18:31 +0530

    Updated

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="ineuron",
+    author_email="sudhich1208@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
